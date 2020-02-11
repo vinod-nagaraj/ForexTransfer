419290c3050ba42879c9925e804b1c578e4475dc
vinod-nagaraj <57495482+vinod-nagaraj@users.noreply.github.com>
2020-02-11 20:26:31 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/fund/transfer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fund/transfer/exception/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.fund.transfer.exception.NotFoundException;
+import com.fund.transfer.exception.ImProperDataException;
+import com.fund.transfer.exception.NoAccountFoundException;
 
 import com.fund.transfer.dto.ErrorDto;
 
@@ -21,6 +24,20 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
 	}
 	
+	@ExceptionHandler(ImProperDataException.class)
+	public ResponseEntity<ErrorDto> ImProperDataException(NotFoundException e){
+		ErrorDto errorDto = new ErrorDto();
+		errorDto.setMessage(e.getMessage());
+		errorDto.setStatusCode(HttpStatus.NOT_FOUND.value());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
+	}
+	@ExceptionHandler(NoAccountFoundException.class)
+	public ResponseEntity<ErrorDto> NoAccountFoundException(NotFoundException e){
+		ErrorDto errorDto = new ErrorDto();
+		errorDto.setMessage(e.getMessage());
+		errorDto.setStatusCode(HttpStatus.NOT_FOUND.value());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
+	}
 	
 	
 }
